568c37a034b829651d307ad554d85076eb37be16
Richard Rhee <rdrhee@gmail.com>
2023-11-12 09:10:00 -0800

    Remove ids from html

--- popup/popup.js
+++
@@ -1,9 +0,0 @@
-function listenForClicks() {
-    document.addEventListener("click", (e)) => {
-        
-    }
-}
-
-browser.tabs.executeScript({file: "/content_scripts/generate_sarcasm.js"})
-.then(listenForClicks)
-.catch(reportExecuteScriptError);
